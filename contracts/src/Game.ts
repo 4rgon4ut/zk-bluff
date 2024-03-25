a9892338eb7b067988c357f56e2506b0c70a1d0f
4rgon4ut <egorlaptev98@gmail.com>
2024-03-25 17:56:07 +0000

    initial

---
+++ contracts/src/Game.ts
@@ -0,0 +1,14 @@
+import { Field, SmartContract, state, State, Poseidon, method } from 'o1js';
+
+
+
+export class Game extends SmartContract {
+    // TODO:
+
+  init() {
+    super.init();
+
+    // TODO:
+  }
+
+}
